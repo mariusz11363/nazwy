70aaaf993d54e2c92722081e91543161d2a1a772
Mariusz Kupczyk <34220053+mariusz11363@users.noreply.github.com>
2018-06-27 12:12:02 +0200

    Create aplikacja.R

---
+++ R/aplikacja.R
@@ -0,0 +1,251 @@
+library("shiny")
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file1", "Wybierz plik CKT", multiple = T),
+      tags$hr(),
+      checkboxInput("header", "Zapis", T),
+      actionButton("do", "Zapisz")
+    ),
+    mainPanel(
+      verbatimTextOutput("zapis"),
+      tableOutput("contents")
+      
+    )
+  )
+)
+
+
+server <- function(input, output) {
+  
+  
+  nazwy <- function(plik_do_poprawy, plik_do_zapisu, zapis=F){
+    
+    library("stringr")
+    library(splitstackshape)
+    library("xlsx")
+    
+    
+    nazwy <- readxl::read_xlsx(plik_do_poprawy, 1, col_names = F)
+    
+    nazwy <- as.data.frame(nazwy)
+    
+    #nazwy <- cSplit(nazwy, 'X__2', sep=" ", type.convert=FALSE)
+    
+    
+    #nazwy <- nazwy[1:500,]
+    
+    for(i in 4:4){
+      
+      cat("Uruchomiono proces...\n")
+      
+      nazwy[i] <- gsub("([0-9])([g])", "\\1 \\2", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([G])", "\\1 g", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([GR])+", "\\1 g", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([gR])+", "\\1 g", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([GB])", "\\1GB", nazwy[[i]])
+      nazwy[i] <- gsub("gR", "g", nazwy[[i]])
+      nazwy[i] <- gsub("gB", "GB", nazwy[[i]])
+      
+      nazwy[i] <- gsub("2 gO", "2GO", nazwy[[i]])
+      nazwy[i] <- gsub("gRAT", "GRAT.", nazwy[[i]])
+      
+      nazwy[i] <- gsub("([0-9])([KG])", "\\1 \\2", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([kg])", "\\1 KG", nazwy[[i]])
+      
+      nazwy[i] <- gsub("([0-9])([L])", "\\1 \\2", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([l])", "\\1 L", nazwy[[i]])
+      
+      nazwy[i] <- gsub("([0-9])([ML])", "\\1 \\2", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([ml])", "\\1 ML", nazwy[[i]])
+      nazwy[i] <- gsub("MLl", "ML", nazwy[[i]])
+      
+      nazwy[i] <- gsub("([0-9])([CM])", "\\1 \\2", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([cm])", "\\1 CM", nazwy[[i]])
+      
+      nazwy[i] <- gsub("([0-9])([MM])", "\\1 \\2", nazwy[[i]])
+      nazwy[i] <- gsub("([0-9])([mm])", "\\1 MM", nazwy[[i]])
+      
+      cat("Gramatury zostały przetworzone.\n")
+      cat("Przetwarzanie jednostek wymiarowych...\n")
+      
+      nazwy[i] <- gsub("kg", "KG", nazwy[[i]])
+      nazwy[i] <- gsub("ml", "ML", nazwy[[i]])
+      # nazwy[i] <- gsub("G", "g", nazwy[[i]])
+      nazwy[i] <- gsub("cm", "CM", nazwy[[i]])
+      nazwy[i] <- gsub("l", "L", nazwy[[i]])
+      nazwy[i] <- gsub("mm", "MM", nazwy[[i]])
+      
+      cat("Jednostki wymiarów zostały opracowane.\n")
+      cat("Trwa przetwarzanie wyjątków...\n")
+      
+      nazwy[i] <- gsub("KAN.SANDWICH", "KANAPKA SANDWICH", nazwy[[i]])
+      nazwy[i] <- gsub("NAP.ŻYWIEC", "NAPÓJ ŻYWIEC", nazwy[[i]])
+      nazwy[i] <- gsub("NEKTARYNA", "NEKTARYNKA", nazwy[[i]])
+      nazwy[i] <- gsub("SAŁ.WIELKANOCNA", "SAŁATKA WIELKANOCNA", nazwy[[i]])
+      nazwy[i] <- gsub("SZYMB.CHLEB", "CHLEB SZYMBARCZANKA", nazwy[[i]])
+      
+      nazwy[i] <- gsub("KROJ.", "KROJONY", nazwy[[i]])
+      
+      nazwy[i] <- gsub("([0-9])([KR])+", "\\1 K", nazwy[[i]])
+      
+      nazwy[i] <- gsub(" KR ","KROJONY", nazwy[[i]])
+      nazwy[i] <- gsub("ZBOŻ","ZBOŻOWY", nazwy[[i]])
+      nazwy[i] <- gsub("OW.TROP.","OWOCE TROPIKALNE", nazwy[[i]])
+      
+      
+      
+      nazwy[i] <- gsub("TAB.MUS.MULTI.","TABLETKI MUSUJĄCE MUTIWITAMINA ", nazwy[[i]])
+      nazwy[i] <- gsub("CYTR-LIM","CYTRYNA LIMONKA", nazwy[[i]])
+      
+      nazwy[i] <- gsub("JAG-LIM-MIĘ","JAGODA LIMONKA MIĘTA", nazwy[[i]])
+      nazwy[i] <- gsub("O SM.","O SMAKU ", nazwy[[i]])
+      nazwy[i] <- gsub("LIM-MIĘT.","LIMONKA MIĘTA ", nazwy[[i]])
+      nazwy[i] <- gsub("CHUST.","CHUSTECZKI ", nazwy[[i]])
+      nazwy[i] <- gsub("PRZYPR.","PRZYPRAWA ", nazwy[[i]])
+      nazwy[i] <- gsub("KIEŁB.","KIEŁBASA ", nazwy[[i]])
+      nazwy[i] <- gsub("DROŻDŻ.","DROŻDŻÓWKA ", nazwy[[i]])
+      nazwy[i] <- gsub("CUK.","CUKIERKI ", nazwy[[i]])
+      nazwy[i] <- gsub("CYG.","CYGARETKI ", nazwy[[i]])
+      nazwy[i] <- gsub("DEO.","DEZODORANT ", nazwy[[i]])
+      nazwy[i] <- gsub("POD PR.","PRYSZNIC ", nazwy[[i]])
+      nazwy[i] <- gsub("INTEN.REGENER.","INTENSE REGENERUJĄCY", nazwy[[i]])
+      nazwy[i] <- gsub("REGENER.","REGENERUJĄCY ", nazwy[[i]])
+      nazwy[i] <- gsub("ENER.","ENERGETYCZNY ", nazwy[[i]])
+      
+      nazwy[i] <- gsub("KAN.TRÓJ","KANAPKA TRÓJKĄTNA", nazwy[[i]])
+      nazwy[i] <- gsub("KAN.TRÓJKAT","KANAPKA TRÓJKĄTNA", nazwy[[i]])
+      nazwy[i] <- gsub("D/NIEMOW.","DLA NIEMOWLĄT ", nazwy[[i]])
+      nazwy[i] <- gsub("D/NIEMO.","DLA NIEMOWLĄT", nazwy[[i]])
+      nazwy[i] <- gsub("D/NIEM.","DLA NIEMOWLĄT", nazwy[[i]])
+      nazwy[i] <- gsub("KGg","KG", nazwy[[i]])
+      nazwy[i] <- gsub("LL","L", nazwy[[i]])
+      nazwy[i] <- gsub("PIEKIEL.","PIEKIELNE ", nazwy[[i]])
+      nazwy[i] <- gsub("KIEL.","KIELISZEK ", nazwy[[i]])
+      nazwy[i] <- gsub("ANTYBAK.","ANTYBAKTERYJNE ", nazwy[[i]])
+      nazwy[i] <- gsub("BROZSK.","BRZOSKWINIA ", nazwy[[i]])
+      nazwy[i] <- gsub("BRZOSK.","BRZOSKWINIA", nazwy[[i]])
+      nazwy[i] <- gsub("KROJONYNE","KROJONE", nazwy[[i]])
+      nazwy[i] <- gsub("BAZ/MIET","BAZYLIA MIĘTA", nazwy[[i]])
+      nazwy[i] <- gsub("ANGIEL.ŻOŁNIER.","ANGIELSKI ŻOŁNIERSKI", nazwy[[i]])
+      nazwy[i] <- gsub("//","/", nazwy[[i]])
+      nazwy[i] <- gsub("ENERGETYCZNY IZER","ENERGIZER", nazwy[[i]])
+      nazwy[i] <- gsub("KIEŁBASA SA","KIEŁBASA", nazwy[[i]])
+      nazwy[i] <- gsub("SPIRYT.","SPIRYTUSOWY ", nazwy[[i]])
+      
+      
+      
+      nazwy[i] <- gsub("LIM-","LIMONKA ", nazwy[[i]])
+      nazwy[i] <- gsub("JABŁ-","JABŁKO ", nazwy[[i]])
+      nazwy[i] <- gsub("WOŁOWOWINĄ","WOŁOWINĄ", nazwy[[i]])
+      nazwy[i] <- gsub("WP GARMAŻ","WIEPRZOWE GARMAŻERYJNE", nazwy[[i]])
+      nazwy[i] <- gsub("HERBATA EXP.","HERBATA EKSPRESOWA ", nazwy[[i]])
+      nazwy[i] <- gsub("KAWA ROZP.","KAWA ROZPUSZCZALNA", nazwy[[i]])
+      nazwy[i] <- gsub("KAWA ZIAR.","KAWA ZIARNISTA ", nazwy[[i]])
+      nazwy[i] <- gsub(" gAT."," GRATIS", nazwy[[i]])
+      nazwy[i] <- gsub("TRZEB.MASŁO EXTRA","Mlekovita Masło ekstra z Trzebowniska", nazwy[[i]])
+      nazwy[i] <- gsub("ŻEBERKA WP","ŻEBERKA WIEPRZOWE", nazwy[[i]])
+      nazwy[i] <- gsub("PO/GOL.","PO GOLENIU ", nazwy[[i]])
+      nazwy[i] <- gsub("ESPR.","ESPRESSO ", nazwy[[i]])
+      nazwy[i] <- gsub("SPR.","SPRAY ", nazwy[[i]])
+      nazwy[i] <- gsub("SEREWTKI","SERWETKI", nazwy[[i]])
+      nazwy[i] <- gsub("SUSZ.","SUSZONE ", nazwy[[i]])
+      nazwy[i] <- gsub("CUKIERKI ERKI","CUKIERKI", nazwy[[i]])
+      nazwy[i] <- gsub("PRZYPRAWA WA","PRZYPRAWA WARZYWNA", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA D/P.","KARMA DLA PSA ", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA D/K.","KARMA DLA KOTA ", nazwy[[i]])
+      nazwy[i] <- gsub("MR CAT D/K","MR CAT DLA KOTA", nazwy[[i]])
+      nazwy[i] <- gsub("GOLONKA WP","GOLONKA WIEPRZOWA", nazwy[[i]])
+      nazwy[i] <- gsub("GULASZ WP","GULASZ WPIERZOWY", nazwy[[i]])
+      nazwy[i] <- gsub("GOLONKI WP"," GOLONKI WIEPRZOWEJ", nazwy[[i]])
+      nazwy[i] <- gsub("SZYNKI WP","SZYNKI WIEPRZOWEJ", nazwy[[i]])
+      nazwy[i] <- gsub("CHUSTECZKI CZKI","CHUSTECZKI", nazwy[[i]])
+      nazwy[i] <- gsub("CHUSTECZKI CZKA","CHUSTECZKI", nazwy[[i]])
+      nazwy[i] <- gsub("HOMOG.","HOMOGENIZOWANY ", nazwy[[i]])
+      nazwy[i] <- gsub("SMAKU KU","SMAKU", nazwy[[i]])
+      nazwy[i] <- gsub("W PANIERZE","W PANIERCE", nazwy[[i]])
+      nazwy[i] <- gsub("EDAL EXP.","EDAL EXPRESOWA ", nazwy[[i]])
+      nazwy[i] <- gsub("EDAL EXP","EDAL EXPRESOWA ", nazwy[[i]])
+      nazwy[i] <- gsub("TABLETKI MUS.","TABLETKI MUSUJĄCE ", nazwy[[i]])
+      nazwy[i] <- gsub("MIOD PODRAVKA","MIÓD PODRAVKA", nazwy[[i]])
+      nazwy[i] <- gsub("SER TW.","SER TWARDY ", nazwy[[i]])
+      nazwy[i] <- gsub("CYGARETKI RETKI","CYGARETKI", nazwy[[i]])
+      nazwy[i] <- gsub("BEZ DOD. CUKIERKI","BEZ DOD. CUKRU", nazwy[[i]])
+      nazwy[i] <- gsub("WHISKAS D/K","WHISKAS DLA KOTA ", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA SHEBA D/K","KARMA SHEBA DLA KOTA ", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA DREAMIES D/K","KARMA DREAMIES DLA KOTA ", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA DARLING D/K.","KARMA DARLING DLA KOTA ", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA PERFECT FIT D/K.","KARMA PERFECT FIT DLA KOTA ", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA GAMA D/K","KARMA GAMA DLA KOTA", nazwy[[i]])
+      nazwy[i] <- gsub("ONE ADULT D/K.","ONE ADULT DLA KOTA", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA FRISKIES D/K","KARMA FRISKIES DLA KOTA", nazwy[[i]])
+      nazwy[i] <- gsub("KARMA WHISK.D/K","KARMA WHISKAS DLA KOTA", nazwy[[i]])
+      nazwy[i] <- gsub(" WP "," WIEPRZ.", nazwy[[i]])
+      #nazwy[i] <- gsub("","", nazwy[[i]])
+      #nazwy[i] <- gsub("","", nazwy[[i]])
+      #nazwy[i] <- gsub("","", nazwy[[i]])
+      #nazwy[i] <- gsub("","", nazwy[[i]])
+      #nazwy[i] <- gsub("","", nazwy[[i]])
+      #nazwy[i] <- gsub("","", nazwy[[i]])
+      
+      
+      
+      cat(" Kolumna nr: ", i,"z", ncol(nazwy), "została opracowana\n")
+      cat("Zakończono...\n\n\n")
+      
+    }
+    
+    if(zapis==TRUE){
+      
+      cat("Rozpoczęto proces zapisu do pliku...\n")
+      #write.csv(x = nazwy, file = lokalizacja_zapisu)
+      writexl::write_xlsx(nazwy, plik_do_zapisu,col_names = F)
+      
+      cat("Zapisano plik w lokalizacji ", plik_do_zapisu)
+    }
+    
+    
+    nazwy
+    
+  }
+  
+  
+  
+  output$contents <- renderTable({
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, it will be a data frame with 'name',
+    # 'size', 'type', and 'datapath' columns. The 'datapath'
+    # column will contain the local filenames where the data can
+    # be found.
+    inFile <<- input$file1
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    nazwy(plik_do_poprawy = inFile$datapath, zapis = F)
+    
+  })
+  
+  observeEvent(input$do, {
+    
+    c <- choose.dir()
+    c <- gsub("\\\\","/", c)
+    
+    
+    
+    nazwy(plik_do_poprawy = inFile$datapath, plik_do_zapisu = paste(paste(c),"/",inFile$name, sep=""), zapis = T)
+    
+    
+    output$zapis <-  renderText({3
+      
+      paste("Zapisano plik w lokalizacji: ", c)
+    })
+    
+    
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
